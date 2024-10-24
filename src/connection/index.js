78f0b4b76bbeaccd081150fd1f92522c6fdb1d31
Afoxcute <jwavoabilove16@gmail.com>
2024-10-24 17:39:02 +0100

    adding files

--- src/connection/index.js
+++ src/connection/index.js
@@ -18,20 +18,18 @@ const BaseMainnet = {
   name: "Base mainnet",
   currency: "ETH",
   explorerUrl: "https://basescan.org/",
-  rpcUrl: import.meta.env.VITE_INFURA_MAINNET_RPC,
+  rpcUrl: import.meta.env.VITE_INFURA_RPC,
 };
 
 const metadata = {
   name: 'My Website',
   description: 'My Website description',
-  url: 'https://localhost:5173',
-  icons: ['https://localhost:5173']
-}
+  url: 'https://localhost:5173',}
 
 export const configWeb3Modal = () => createWeb3Modal({
   ethersConfig: defaultConfig({ metadata }),
   chains: [BaseSepolia, BaseMainnet],
-  projectId: import.meta.env.VITE_PROJECTID,
+  projectId: import.meta.env.VITE_PROJECT_ID,
   enableAnalytics: false,
   themeVariables: {
     '--w3m-accent': '#3F9AAE',
