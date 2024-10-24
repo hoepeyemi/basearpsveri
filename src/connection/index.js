74a6fef27cc9bfe10ade90c9ba8c2472f8a2354e
Afoxcute <jwavoabilove16@gmail.com>
2024-10-24 18:22:40 +0100

    adding files

--- src/connection/index.js
+++ src/connection/index.js
@@ -13,13 +13,13 @@ const BaseSepolia = {
   rpcUrl: import.meta.env.VITE_INFURA_RPC,
 };
 
-const BaseMainnet = {
-  chainId: 8453,
-  name: "Base mainnet",
-  currency: "ETH",
-  explorerUrl: "https://basescan.org/",
-  rpcUrl: import.meta.env.VITE_INFURA_RPC,
-};
+// const BaseMainnet = {
+//   chainId: 8453,
+//   name: "Base mainnet",
+//   currency: "ETH",
+//   explorerUrl: "https://basescan.org/",
+//   rpcUrl: import.meta.env.VITE_INFURA_RPC,
+// };
 
 const metadata = {
   name: 'My Website',
