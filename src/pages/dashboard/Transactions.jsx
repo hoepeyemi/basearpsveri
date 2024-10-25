d8f062b83696cac93ed3f28bc3bcab33cd337ea5
Afoxcute A <Jwavolabilove16@gmail.com>
2024-10-25 15:17:00 +0000

    completed

--- src/pages/dashboard/Transactions.jsx
+++ src/pages/dashboard/Transactions.jsx
@@ -68,7 +68,7 @@ const Transactions = () => {
     <main>
        <section className='flex flex-col lg:flex-row md:flex-row bg-[#0C3B45] rounded-[20px] w-[100%] text-white'>
         <div className='lg:w-[60%] md:w-[60%] w-[100%] p-8'>
-            <h2 className='lg:text-[24px] md:text-[24px] text-[18px] font-bold mb-4'>GreeenEarns - Where environmental consciousness meets blockchain innovation</h2>
+            <h2 className='lg:text-[24px] md:text-[24px] text-[18px] font-bold mb-4'>GreeenEarns -where e-commerce helps the needy</h2>
             <p>View all your eco-friendly product purchases in one place. Track your contributions to a greener planet with each sustainable product you buy.</p>
             <div className='mt-6'>
             <button onClick={() => navigate('/dashboard/marketplace')}  className="bg-white text-[#0C3B45] py-2 px-4 rounded-lg lg:text-[20px] md:text-[20px] font-bold text-[16px] lg:w-[50%] md:w-[50%] w-[100%] my-2 hover:bg-[#C7D5D8] hover:font-bold">Buy Product</button>
